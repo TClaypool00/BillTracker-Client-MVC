7135f9906944009364eef79889d6d07d25187c50
claypooltyler00 <claypooltyler00@gmail.com>
2022-06-09 18:56:22 -0500

    Created new MVC project

---
+++ BillTrackerClient.App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BillTrackerClient.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
