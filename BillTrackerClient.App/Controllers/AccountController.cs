fe1af2290301202c1cf00221f4035e39632079da
claypooltyler00 <claypooltyler00@gmail.com>
2022-06-09 20:03:51 -0500

    Views and routes for Login and Regiser page

---
+++ BillTrackerClient.App/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using BillTrackerClient.App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BillTrackerClient.App.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Login(LoginModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Register(RegisterModel model)
+        {
+            return View();
+        }
+    }
+}
