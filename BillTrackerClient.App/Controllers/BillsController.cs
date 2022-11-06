04fb733fa5648917143b78bd4a6fe85565c24916
TClaypool00 <claypooltyler00@gmail.com>
2022-11-05 21:34:40 -0500

    Added Add Bill page. No Post method yet

---
+++ BillTrackerClient.App/Controllers/BillsController.cs
@@ -0,0 +1,15 @@
+﻿using BillTrackerClient.App.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BillTrackerClient.App.Controllers
+{
+    public class BillsController : ControllerHelper
+    {
+        [HttpGet]
+        [Authenitcation]
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
