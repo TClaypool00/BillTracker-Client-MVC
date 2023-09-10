193f7eb4f1f039528e86196e65147bb868553e74
TClaypool00 <claypooltyler00@gmail.com>
2023-09-09 22:44:45 -0500

    Removed unneccessary methods

--- BillTrackerClient.App/Controllers/ControllerHelper.cs
+++ BillTrackerClient.App/Controllers/ControllerHelper.cs
@@ -1,74 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace BillTrackerClient.App.Controllers
 {
     public class ControllerHelper : Controller
-    {
-        protected int UserId
-        {
-            get
-            {
-                return (int)HttpContext.Session.GetInt32("UserId");
-            }
-        }
-
-        protected string FirstName
-        {
-            get
-            {
-                return GetSessionName("FirstName");
-            }
-        }
-
-        protected string LastName
-        {
-            get
-            {
-                return GetSessionName("LastName");
-            }
-        }
-
-        protected string Email
-        {
-            get
-            {
-                return GetSessionName("Email");
-            }
-        }
-
-        protected bool IsAdmin
-        {
-            get
-            {
-                return GetSessionName("IsAdmin") == "True";
-            }
-        }
-
-        protected string PhoneNumber
-        {
-            get
-            {
-                return GetSessionName("PhoneNum");
-            }
-        }
-
-        protected string ErrorMessage
-        {
-            get
-            {
-                return "Ann error has occured";
-            }
-        }
-        
+    {   
         protected ActionResult Home()
         {
             return RedirectToAction("Index", "Home");
-        }
-
-        private string GetSessionName(string keyName)
-        {
-            return HttpContext.Session.GetString(keyName);
         }
     }
 }
