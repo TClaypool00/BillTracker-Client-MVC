9d08019e4df737154104ae7374568994f8fc13f2
TClaypool00 <claypooltyler00@gmail.com>
2023-09-11 00:46:52 -0500

    Created a helper for controller classes

--- BillTrackerClient.App/Controllers/ControllerHelper.cs
+++ BillTrackerClient.App/Controllers/ControllerHelper.cs
@@ -1,12 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace BillTrackerClient.App.Controllers
 {
     public class ControllerHelper : Controller
-    {   
+    {
+        #region Protected methods
+        #region ActionResult methods
         protected ActionResult Home()
         {
             return RedirectToAction("Index", "Home");
         }
+
+        protected ActionResult SetError(string errorMessage, object model)
+        {
+            ViewBag.error = errorMessage;
+
+            return View(model);
+        }
+        #endregion
+
+        #region void methods
+        protected void SetError(Exception exception)
+        {
+            ViewBag.error = exception.Message;
+        }
+
+        protected void SetTempSuccessMessage(string message)
+        {
+            TempData["Success"] = message;
+        }
+
+        protected void GetTempSuccessMessage()
+        {
+            if (TempData["Success"] is not null)
+            {
+                ViewBag.success = TempData["Success"];
+            }
+        }
+        #endregion
+
+        #endregion
     }
 }
