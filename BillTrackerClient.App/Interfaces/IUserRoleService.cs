ac167fac9d605df51240f133a642427fe05409a8
TClaypool00 <claypooltyler00@gmail.com>
2023-09-11 00:38:25 -0500

    Service for User roles tables

---
+++ BillTrackerClient.App/Interfaces/IUserRoleService.cs
@@ -0,0 +1,11 @@
+﻿using BillTrackerClient.App.CoreModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BillTrackerClient.App.Interfaces
+{
+    public interface IUserRoleService
+    {
+        public Task<List<CoreRole>> GetRoleNamesByUserIdAsync(int userId);
+    }
+}
---
+++ BillTrackerClient.App/Services/UserRoleService.cs
@@ -0,0 +1,45 @@
+﻿using BillTrackerClient.App.CoreModels;
+using BillTrackerClient.App.DataModels;
+using BillTrackerClient.App.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BillTrackerClient.App.Services
+{
+    public class UserRoleService : ServiceHelper, IUserRoleService
+    {
+        public UserRoleService(BillTrackerContext context) : base(context)
+        {
+        }
+
+        public async Task<List<CoreRole>> GetRoleNamesByUserIdAsync(int userId)
+        {
+            var coreRoles = new List<CoreRole>();
+            var dataUserRoles = await _context.UserRoles
+                .Where(u => u.UserId == userId)
+                .Select(r => new UserRole
+                {
+                    Role = new Role
+                    {
+                        RoleId = r.Role.RoleId,
+                        RoleName = r.Role.RoleName,
+                    }
+                }).ToListAsync();
+
+            if (dataUserRoles.Count > 0)
+            {
+                Role role;
+                for (int i = 0; i < dataUserRoles.Count; i++)
+                {
+                    role = dataUserRoles[i].Role;
+
+                    coreRoles.Add(new CoreRole(role));
+                }
+            }
+
+            return coreRoles;
+        }
+    }
+}
