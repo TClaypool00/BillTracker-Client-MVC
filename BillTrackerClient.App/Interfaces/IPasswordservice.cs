3e2f3eac414d8041f9b2ed8ef384e739a1752265
TClaypool00 <claypooltyler00@gmail.com>
2023-09-11 00:42:39 -0500

    Created a service to verify and hash a password. Service also checks to see if it meets the requiremnts

---
+++ BillTrackerClient.App/Interfaces/IPasswordservice.cs
@@ -0,0 +1,17 @@
+﻿namespace BillTrackerClient.App.Interfaces
+{
+    public interface IPasswordservice
+    {
+        #region public methods
+        public string HashPassword(string password);
+
+        public bool VerifyPassword(string hash, string password);
+
+        public bool PasswordMeetsRequirements(string password);
+        #endregion
+
+        #region public properties
+        public string PasswordDoesNotMeetRequirements { get; }
+        #endregion
+    }
+}
---
+++ BillTrackerClient.App/Services/PasswordService.cs
@@ -0,0 +1,25 @@
+﻿using BillTrackerClient.App.Interfaces;
+using System.Text.RegularExpressions;
+
+namespace BillTrackerClient.App.Services
+{
+    public class PasswordService : IPasswordservice
+    {
+        public string PasswordDoesNotMeetRequirements => "Password does not meet our requirements.";
+
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool PasswordMeetsRequirements(string password)
+        {
+            return Regex.IsMatch(password, @"^(.{8,20}|[^0-9]*|[^A-Z])$");
+        }
+
+        public bool VerifyPassword(string hash, string password)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}
