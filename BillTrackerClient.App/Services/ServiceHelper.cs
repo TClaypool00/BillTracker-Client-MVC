04ec75e42eeadf358f496bda91de371ea6f2e1d3
TClaypool00 <claypooltyler00@gmail.com>
2023-09-11 00:43:10 -0500

    Created a helper for Services classes

---
+++ BillTrackerClient.App/Services/ServiceHelper.cs
@@ -0,0 +1,20 @@
+﻿using BillTrackerClient.App.DataModels;
+using System.Threading.Tasks;
+
+namespace BillTrackerClient.App.Services
+{
+    public class ServiceHelper
+    {
+        protected readonly BillTrackerContext _context;
+
+        public ServiceHelper(BillTrackerContext context)
+        {
+            _context = context;
+        }
+
+        protected async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
