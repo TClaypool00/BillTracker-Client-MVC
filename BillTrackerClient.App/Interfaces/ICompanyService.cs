b8e57d03facd055721279360c15deeecf3189aa6
TClaypool00 <claypooltyler00@gmail.com>
2023-09-12 09:45:59 -0500

    Created service for company

---
+++ BillTrackerClient.App/Interfaces/ICompanyService.cs
@@ -0,0 +1,14 @@
+﻿using BillTrackerClient.App.CoreModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BillTrackerClient.App.Interfaces
+{
+    public interface ICompanyService
+    {
+        public Task<List<SelectListItem>> GetCompanyDropDownAsync(int? index = null);
+
+        public Task<CoreCompany> AddCompanyAsync(CoreCompany company);
+    }
+}
---
+++ BillTrackerClient.App/Services/CompanyService.cs
@@ -0,0 +1,32 @@
+﻿using BillTrackerClient.App.CoreModels;
+using BillTrackerClient.App.DataModels;
+using BillTrackerClient.App.Interfaces;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BillTrackerClient.App.Services
+{
+    public class CompanyService : ServiceHelper, ICompanyService
+    {
+        public CompanyService(BillTrackerContext context) : base(context)
+        {
+        }
+
+        public async Task<CoreCompany> AddCompanyAsync(CoreCompany company)
+        {
+            var dataCompany = new Company(company);
+            await _context.Companies.AddAsync(dataCompany);
+            await SaveAsync();
+
+            company.CompanyId = dataCompany.CompanyId;
+
+            return company;
+        }
+
+        public Task<List<SelectListItem>> GetCompanyDropDownAsync(int? index = null)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
