10c52135bca63be8e634437c7a03777b4bcae9b2
TClaypool00 <claypooltyler00@gmail.com>
2023-09-12 09:44:16 -0500

    Added companies table

---
+++ BillTrackerClient.App/DataModels/Company.cs
@@ -0,0 +1,40 @@
+﻿using BillTrackerClient.App.CoreModels;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BillTrackerClient.App.DataModels
+{
+    public class Company
+    {
+        private readonly CoreCompany _company;
+
+        public Company()
+        {
+
+        }
+
+        public Company(CoreCompany company)
+        {
+            _company = company ?? throw new ArgumentNullException(nameof(company));
+
+            if (_company.CompanyId > 0)
+            {
+                CompanyId = _company.CompanyId;
+            }
+
+            CompanyName = _company.CompanyName;
+            UserId = _company.UserId;
+        }
+
+        [Key]
+        public int CompanyId { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string CompanyName { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ BillTrackerClient.App/Migrations/20230912042811_AddedCompanies.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BillTrackerClient.App.Migrations
+{
+    public partial class AddedCompanies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    CompanyId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CompanyName = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.CompanyId);
+                    table.ForeignKey(
+                        name: "FK_Companies_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_UserId",
+                table: "Companies",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
