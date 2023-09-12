27ef8c0081a72e311d8ab49ae23da2b5d01a2274
TClaypool00 <claypooltyler00@gmail.com>
2023-09-12 09:45:35 -0500

    Created an data validation annotaion to check if the date is in the current month

---
+++ BillTrackerClient.App/CustomAnnotations/DateMustPresent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BillTrackerClient.App.CustomAnnotations
+{
+    public class DateMustPresent : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime date)
+            {
+                if (date > DateTime.Today.AddMonths(-1) && date < DateTime.Today)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Value must be a DateTime");
+            }
+        }
+    }
+}
