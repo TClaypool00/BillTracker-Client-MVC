e77077b683ccde8cb82b8889787796d9bf8da8ed
TClaypool00 <claypooltyler00@gmail.com>
2023-09-12 09:47:08 -0500

    Created a view and controller for adding a bill

---
+++ BillTrackerClient.App/Controllers/BillsController.cs
@@ -0,0 +1,31 @@
+﻿using BillTrackerClient.App.Models.PostModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace BillTrackerClient.App.Controllers
+{
+    [Authorize]
+    public class BillsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var model = new PostBillViewModel()
+            {
+                CompanyDropDown = new List<SelectListItem>()
+                {
+                    new SelectListItem("Add a company", "")
+                }
+            };
+
+            return View(model);
+        }
+
+        public ActionResult AddAsync(PostBillViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
