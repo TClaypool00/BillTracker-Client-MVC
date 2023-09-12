2693f62fe2475b71a6766f7c28448ad3f3a64bd2
TClaypool00 <claypooltyler00@gmail.com>
2023-09-12 09:44:35 -0500

    Created core model for company

---
+++ BillTrackerClient.App/CoreModels/CoreCompany.cs
@@ -0,0 +1,12 @@
+﻿namespace BillTrackerClient.App.CoreModels
+{
+    public class CoreCompany
+    {
+        public int CompanyId { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public int UserId { get; set; }
+        public CoreUser User { get; set; }
+    }
+}
