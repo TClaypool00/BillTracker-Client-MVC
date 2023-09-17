6c53b353346e3f51e64fcf9444e8c30bf8a76fd9
claypooltyler00 <claypooltyler00@gmail.com>
2023-09-16 19:53:54 -0500

    Added Create method for bill service

---
+++ BillTrackerClient.App/Interfaces/IBillService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BillTrackerClient.App.Interfaces
+{
+    public interface IBillService
+    {
+        #region Public methods
+        public Task<bool> BillNameExistsAsync(string name, int userId);
+
+        public Task CreateBillAsync();
+        #endregion
+    }
+}
