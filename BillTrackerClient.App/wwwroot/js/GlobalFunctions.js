57035b1f7b95d7d2d363f7e7ef8fadb9196c95ee
TClaypool00 <claypooltyler00@gmail.com>
2023-09-24 00:22:36 -0500

    Dispalying JS error messages is now its own file and function

---
+++ BillTrackerClient.App/wwwroot/js/GlobalFunctions.js
@@ -0,0 +1,17 @@
+﻿function displayErrorMessage(err) {
+    let jsonError = err.responseJSON
+
+    if (jsonError) {
+        if (Array.isArray(jsonError)) {
+            for (let a = 0; a < jsonError.length; a++) {
+                const item = jsonError[a];
+                const listItem = document.createElement('li');
+                listItem.innerHTML = item;
+
+                errorList.appendChild(listItem);
+            }
+        }
+    } else {
+        errorMessage.innerHTML = err.responseText;
+    }
+}
