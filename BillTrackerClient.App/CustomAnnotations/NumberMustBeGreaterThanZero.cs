7ed2bf3a08a7793d35ae2084c3e53763ad8b585d
TClaypool00 <claypooltyler00@gmail.com>
2023-09-24 00:09:25 -0500

    Created custom data annoation to check if a number is greater than 0

---
+++ BillTrackerClient.App/CustomAnnotations/NumberMustBeGreaterThanZero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BillTrackerClient.App.CustomAnnotations
+{
+    public class NumberMustBeGreaterThanZero : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is int number)
+            {
+                if (number > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    ErrorMessage = "Must be greater than 0";
+
+                    return false;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Must be an integer");
+            }
+        }
+    }
+}
