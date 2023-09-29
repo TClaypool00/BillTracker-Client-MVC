05741d3e30137aaca6f9d0ddb9fb97876f5065f4
TClaypool00 <claypooltyler00@gmail.com>
2023-09-28 22:11:36 -0500

    Added a base model for the view models to reduce code redudency

---
+++ BillTrackerClient.App/Models/BaseModels/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BillTrackerClient.App.Models.BaseModels
+{
+    public abstract class BaseViewModel
+    {
+        #region Public Property
+        [Display(Name = "Date Created")]
+        public string DateCreated { get; set; }
+
+        [Display(Name = "Price")]
+        public string PriceString { get; set; }
+
+        public string Company { get; set; }
+
+        [Display(Name = "Date paid")]
+        public string DatePaid { get; set; }
+
+        [Display(Name = "Date Due")]
+        public string DateDueString { get; set; }
+        #endregion
+    }
+}
