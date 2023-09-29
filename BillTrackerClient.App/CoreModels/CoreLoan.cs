f4c04f38cda6fe8f4e3ee65073ba4ffb881b9dcd
TClaypool00 <claypooltyler00@gmail.com>
2023-09-29 18:51:39 -0500

    Created CoreLoan class with basic functionality

---
+++ BillTrackerClient.App/CoreModels/CoreLoan.cs
@@ -0,0 +1,20 @@
+﻿using BillTrackerClient.App.CoreModels.AbstractModels;
+
+namespace BillTrackerClient.App.CoreModels
+{
+    public class CoreLoan : CoreExpense
+    {
+        #region Constructors
+        public CoreLoan()
+        {
+
+        }
+        #endregion
+
+        #region Public properties
+        public int LoanId { get; set; }
+
+        public string LoanName { get; set; }
+        #endregion
+    }
+}
