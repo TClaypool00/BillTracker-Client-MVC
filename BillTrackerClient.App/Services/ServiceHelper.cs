83a31c7db8506bbe45780e1f6bea37e7e8df46f6
TClaypool00 <claypooltyler00@gmail.com>
2023-09-28 22:14:49 -0500

    Added toggle message success on the back end

--- BillTrackerClient.App/Services/ServiceHelper.cs
+++ BillTrackerClient.App/Services/ServiceHelper.cs
@@ -1,5 +1,7 @@
 ﻿using BillTrackerClient.App.DataModels;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Threading.Tasks;
 
 namespace BillTrackerClient.App.Services
@@ -8,6 +10,8 @@ namespace BillTrackerClient.App.Services
     {
         protected readonly BillTrackerContext _context;
         protected readonly string _modelString;
+        protected readonly IConfiguration _configuration;
+        protected readonly int _standardTakeValue;
 
         protected int _index;
 
@@ -16,10 +20,12 @@ namespace BillTrackerClient.App.Services
             _context = context;
         }
 
-        public ServiceHelper(BillTrackerContext context, string modelName)
+        public ServiceHelper(BillTrackerContext context, IConfiguration configuration, string modelName)
         {
             _context = context;
             _modelString = modelName;
+            _configuration = configuration;
+            _standardTakeValue = int.Parse(_configuration.GetSection("TakeValues").GetSection("Standard").Value);
         }
 
         protected async Task SaveAsync()
@@ -55,6 +61,29 @@ namespace BillTrackerClient.App.Services
         public string ModelAlreadyExistsMessage(string model, string name)
         {
             return $"A {model} with the name {name} aready exists.";
+        }
+
+        protected string ToggleIsActiveMssage(bool isActive)
+        {
+            if (string.IsNullOrEmpty(_modelString))
+            {
+                throw new ApplicationException("Cannot use this method");
+            }
+            else
+            {
+                string message = $"{_modelString} has been made ";
+
+                if (isActive)
+                {
+                    message += "active";
+                }
+                else
+                {
+                    message += "inactive";
+                }
+
+                return message;
+            }
         }
     }
 }
