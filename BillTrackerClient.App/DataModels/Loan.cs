695aa53d2e38acb63a64107133e2e02dfffb9228
TClaypool00 <claypooltyler00@gmail.com>
2023-09-29 19:35:26 -0500

    Loan class has constructors for CoreLoan model

--- BillTrackerClient.App/DataModels/Loan.cs
+++ BillTrackerClient.App/DataModels/Loan.cs
@@ -3,11 +3,41 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using BillTrackerClient.App.CoreModels;
 
 namespace BillTrackerClient.App.DataModels
 {
     public class Loan
     {
+        #region Private fields
+        #region Private model fields
+        private readonly CoreLoan _coreLoan;
+        #endregion
+        #endregion
+
+        #region Constructors
+        public Loan()
+        {
+
+        }
+
+        public Loan(CoreLoan coreLoan)
+        {
+            _coreLoan = coreLoan ?? throw new ArgumentNullException(nameof(coreLoan));
+
+            if (_coreLoan.LoanId > 0)
+            {
+                LoanId = _coreLoan.LoanId;
+            }
+
+            LoanName = _coreLoan.LoanName;
+            IsActive = _coreLoan.IsActive;
+            UserId = _coreLoan.UserId;
+            CompanyId = _coreLoan.CompanyId;
+        }
+        #endregion
+
+        #region Public properties
         [Key]
         public int LoanId { get; set; }
 
@@ -37,5 +67,6 @@ namespace BillTrackerClient.App.DataModels
         public PaymentHistory PaymentHistory { get; set; }
 
         public List<PaymentHistory> PaymentHistories { get; set; }
+        #endregion
     }
 }
